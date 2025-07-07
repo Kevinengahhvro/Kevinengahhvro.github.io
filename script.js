80ddb84959802d461f09bde9cc89baa02efce6b4
Kevinengahhvro <k.nicholasanand@gmail.com>
2025-07-07 14:07:11 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const magneticBg = document.querySelector('.magnetic-bg');
+
+document.addEventListener('mousemove', (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    magneticBg.style.background = `radial-gradient(circle at ${x}px ${y}px, rgba(0, 150, 255, 0.3) 0%, transparent 50%)`;
+});
 
